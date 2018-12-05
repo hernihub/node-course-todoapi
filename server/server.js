e63464c057430fb186e6832cec80d9e56d0dd37e
hernihub <hernan_quevedo@yahoo.com>
2018-12-05 12:59:12 -0500

    Get /todos route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,16 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos}); // flexibility: instead of sending back and array, an object is sent
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, () => {
     console.log('Started on port 3000');
 });
+
+module.exports = {app};
