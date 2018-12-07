2df0d8fa50e808e40266428a96624dc7f2d2c295
hernihub <hernan_quevedo@yahoo.com>
2018-12-06 19:18:57 -0500

    Test the delete todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -99,3 +99,40 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString();
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+        .end((err, res) => {
+            if (err) {
+                return done(err); // tells mocha to render the error
+            }
+            Todo.findById(hexId).then((todo) => {
+                expect(todo).toBeFalsy();
+                done();
+            }).catch((e) => {done(e);});
+        });
+    });
+
+    it('should return a 404 if todo not found', (done) => {
+        var hexId = new ObjectID().toHexString();
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404)
+            .end(done);
+    });
+
+    it('should return a 404 if objectId is invalid', (done) => {
+        request(app)
+            .delete('/todos/123anb')
+            .expect(404)
+            .end(done); 
+    });
+});
